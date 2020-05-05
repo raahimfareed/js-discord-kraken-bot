4f6da2771b4743bf29230c5ff05278de9ca80133
Raahim Fareed <raahim1965@hotmail.com>
2020-05-05 19:17:52 +0500

    Translator

--- translate.js
+++
@@ -1,7 +0,0 @@
-const translate = require('@vitalets/google-translate-api');
-
-translate('I speak Spanish', {from: 'en', to: 'es'}).then(res => {
-    console.log(res);
-}).catch(err => {
-    console.error(err);
-});
